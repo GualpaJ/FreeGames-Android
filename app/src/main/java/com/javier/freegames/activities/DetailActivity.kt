750eb35eba3b30557f914bfa93c59f36a6ac80ef
GualpaJ <javier9610@hotmail.es>
2026-04-23 16:20:12 +0200

    FEAT: Navegación a pantalla detalle del juego con scroll en el contenido de la descripción

---
+++ app/src/main/java/com/javier/freegames/activities/DetailActivity.kt
@@ -0,0 +1,63 @@
+package com.javier.freegames.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.javier.freegames.data.Game
+import com.javier.freegames.databinding.ActivityDetailBinding
+import android.content.Intent
+import android.net.Uri
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.javier.freegames.R
+import com.javier.freegames.data.GameService
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import androidx.core.net.toUri
+
+
+class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailBinding
+
+    lateinit var game: Game
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val id = intent.getIntExtra("GAME_ID", -1)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            game = GameService.getInstance().getGameById(id)
+
+            CoroutineScope(Dispatchers.Main).launch {
+                loadData()
+            }
+        }
+    }
+
+    fun loadData() {
+        supportActionBar?.title = game.title
+
+        binding.titleTextView.text = game.title
+        Picasso.get().load(game.image).into(binding.thumbnailImageView)
+        binding.gameUrlButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(game.gameURL.toUri())
+            startActivity(intent)
+        }
+        binding.descriptionTextView.text = game.description
+    }
+}
